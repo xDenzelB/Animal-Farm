11c7ceb7daf36489fa30de27891b5e75b487510f
Denzel <dbartolaba123@gmail.com>
2021-11-05 08:54:08 -0700

    Animal Farm

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-// import functions and grab DOM elements
+const doggie = document.getElementById ('dogPic'); 
+const dog = document.getElementById ('dogSound');
+const tagDog = document.getElementById ('DOG');
+// dog section 
+console.log(doggie);
+console.log(dog);
+console.log(tagDog);
 
-// initialize global state
+const kitty = document.getElementById ('catPic');
+const Cat = document.getElementById ('catSound');
+const tagCat = document.getElementById ('CAT');
+// cat section
+console.log(kitty);
+console.log(Cat);
+console.log(tagCat);
+const horsey = document.getElementById ('horsePic');
+const horse = document.getElementById ('horseSound');
+const tagHorse = document.getElementById ('HORSE');
+// horse section
+console.log(horsey);
+console.log(horse);
+console.log(tagHorse);
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+doggie.addEventListener('click', () =>{
+    dog.play();
+    tagDog.textContent = 'DOG';
+});
+
+kitty.addEventListener('click', () =>{
+    Cat.play();
+    tagCat.textContent = 'CAT';
+});
+
+horsey.addEventListener('click', () =>{
+
+    horse.play();
+    tagHorse.textContent = 'HORSE';
+});
